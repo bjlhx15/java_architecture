f7611d650613c93d8028feab7a59d5c63388acbd
bjlhx15 <bjlhx15@163.com>
2019-06-03 20:50:40 +0800

    web

---
+++ architecture1/customermgr/src/main/java/com/github/bjlhx15/architecture/customermgr/web/CustomerController.java
@@ -0,0 +1,10 @@
+package com.github.bjlhx15.architecture.customermgr.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping(value = "/customer")
+public class CustomerController {
+}
